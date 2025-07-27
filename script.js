c373fc1ac669e5b222e7ba6093cfd2e41a35f133
mcalsing <marcelocalsing@gmail.com>
2025-07-27 00:30:05 -0300

    feat: created function to show filtered products when search input is filled

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 const API_URL = 'http://localhost:3000/products';
 
 const card = document.getElementById('card');
+//const searchBar = document.getElementById('search-bar').value;
 
+var products;
 
 const getProduct = async () => {
-  const {data} = await axios.get(API_URL);
-  
+  const response = await axios.get(API_URL);
+  products = response.data;
+
   card.innerHTML = '';
 
-  data.forEach(product => {
+  products.forEach(product => {
+    card.innerHTML += `
+      <div class="w-60 h-85 border-2 border-[#d1d1ad] rounded-md">
+        <div class="h-60 flex justify-center items-center bg-[#ededde] rounded-t-md">
+          <img class="w-40 h-fit" src="${product.image}" alt="">
+        </div>
+        <div class="flex flex-col text-[#727240] py-4 px-1 gap-2">
+          <span class="font-medium text-xl">${product.name}</span>
+          <span>R$ ${product.price}</span>
+        </div>
+      </div>
+    `
+  })
+
+  
+}
+window.onload = getProduct;
+
+const showFilteredProducts = (inputFilter) => {
+  card.innerHTML = '';
+
+  const filteredProducts = products.filter(p => p.name.toLowerCase().includes(inputFilter.toLowerCase()))
+
+  filteredProducts.forEach(product => {
     card.innerHTML += `
       <div class="w-60 h-85 border-2 border-[#d1d1ad] rounded-md">
         <div class="h-60 flex justify-center items-center bg-[#ededde] rounded-t-md">
@@ -22,4 +48,3 @@ const getProduct = async () => {
     `
   })
 }
-window.onload = getProduct;
