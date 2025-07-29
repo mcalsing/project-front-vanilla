4a6c8f62d0a508170985274907abb1f90bb79984
mcalsing <marcelocalsing@gmail.com>
2025-07-29 11:41:06 -0300

    feat: Change colors in modal and checkout page

--- script.js
+++ script.js
@@ -81,20 +81,20 @@ const openModal = (image, name, price, stock) => {
   modal.showModal();
   modal.innerHTML = `
     <div class="w-200 h-120 flex rounded-md">
-      <div class="flex items-center justify-center w-100 h-full p-10 bg-slate-200 w-1/2">
+      <div class="flex items-center justify-center w-100 h-full p-10 bg-[#ededde] w-1/2">
         <img src="${image}" alt="">
       </div>
 
       <div class="flex flex-col px-10 gap-1 w-1/2">
-        <span onclick="closeModal()" class="ml-auto pt-3 text-slate-600 cursor-pointer">x</span>
+        <span onclick="closeModal()" class="ml-auto pt-3 cursor-pointer">x</span>
         <h1 class="text-3xl font-bold">${name}</h1>
-        <span class="px-3 p-1 rounded-2xl bg-slate-200 w-fit text-xs">TOTAL IN STOCK: ${stock}</span>
+        <span class="px-3 p-1 rounded-2xl bg-[#ededde] w-fit text-xs">TOTAL IN STOCK: ${stock}</span>
         <span class="text-xs font-medium mt-10">PRICE</span>
         <span class="text-xl"> $ ${price}</span>
 
           <div class="mt-10">
             <h1 class="text-xs font-medium mb-2">QUANTITY</h1>
-            <div class="flex border-1 border-slate-400 rounded h-11 justify-between w-41 items-center mb-12 px-4">
+            <div class="flex border-1 border-[#d1d1ad] rounded h-11 justify-between w-41 items-center mb-12 px-4">
               <span class="cursor-pointer" onclick="decrementAmount(${stock})">
                 <img src="/assets/minus.svg" alt="" />
               </span>
@@ -105,7 +105,7 @@ const openModal = (image, name, price, stock) => {
             </div>
 
             <button 
-              class="bg-gray-400 rounded-md w-full text-white cursor-pointer px-6 py-3"
+              class="bg-lime-900 rounded-md w-full text-white cursor-pointer px-6 py-3"
               onclick="addProductToCart('${image}', '${name}', ${price})"
             >
               Add to cart
@@ -169,7 +169,6 @@ const currentURL = window.location.pathname
 
 const checkoutProducts = document.getElementById('orders');
 
-
 const showCheckoutProducts = () => {
   var total = document.getElementById('total')
 
@@ -193,7 +192,7 @@ const showCheckoutProducts = () => {
     //Listegem do produtos
     checkoutProducts.innerHTML += `
       <div class="flex items-center gap-10">
-        <div class="flex h-20 bg-slate-200 items-center">
+        <div class="flex h-20 bg-[#ededde] items-center">
           <img class="w-15 h-fit flex" src="${item.image}" alt="">
         </div>
         <div class="flex flex-col gap-1">
@@ -201,7 +200,7 @@ const showCheckoutProducts = () => {
           <span class="text-sm">quantity: ${item.quantity}</span>
         </div>
         <span class="text-xl ml-10 ml-auto">$ ${item.price}</span>
-        <div class="w-8 h-8 bg-slate-200 rounded-sm cursor-pointer">
+        <div class="w-8 h-8 bg-[#ededde] rounded-sm cursor-pointer">
           <img class="px-2 py-2" src="/assets/close.svg" alt="" />
         </div>
       </div>
